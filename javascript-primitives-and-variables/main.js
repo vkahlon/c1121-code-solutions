7b6c8d0345c593756434bba5ccb64e4884625aa2
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-06 10:29:30 -0800

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Vickram Kahlon';
+var isCool = false;
+var totalPets = 1;
+var unknown = null;
+var never;
+
+console.log('value of fullName is', fullName);
+console.log('value of isCool is', isCool);
+console.log('value of totalPets is', totalPets);
+
+console.log('typeof fullName', typeof fullName);
+console.log('typeof isCool', typeof isCool);
+console.log('typeof totalPets', typeof totalPets);
+
+console.log('value of unknown:', unknown);
+console.log('typeof unknown', typeof unknown);
+
+console.log('value of never:', never);
+console.log('typeof never', typeof never);
