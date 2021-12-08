2c2e6536624ac973d0dcee8694b3a5715571df72
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-08 10:01:52 -0800

    javascript-functions-practice.

---
+++ javascript-functions-practice/get-sum-of-squares.js
@@ -0,0 +1,7 @@
+/* exported getSumOfSquares */
+function getSumOfSquares(x, y) {
+  var squareOne = x * x;
+  var squareTwo = y * y;
+  var sumOfSquares = squareOne + squareTwo;
+  return sumOfSquares;
+}
