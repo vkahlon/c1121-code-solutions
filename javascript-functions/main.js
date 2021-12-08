6b3f303af0c30915df9b9c24cbf0dd4f63f5b940
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-07 17:26:47 -0800

    Completed javascript-functions.

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -2,28 +2,28 @@ function convertMinutesToSeconds(minutes) {
   var seconds = minutes * 60;
   return seconds;
 }
-console.log(convertMinutesToSeconds(5));
+console.log('Value of convertMinutesToSeconds: ' + convertMinutesToSeconds(5));
 
 function greet(name) {
   var greeting = 'Hey, ' + name;
   return greeting;
 }
-console.log(greet('Beavis'));
+console.log('Value of greet: ' + greet('Beavis'));
 
 function getArea(width, height) {
   var area = width * height;
   return area;
 }
-console.log(getArea(17, 42));
+console.log('Value of getArea: ' + getArea(17, 42));
 
 function getFirstName(person) {
   var frenchName = person;
   return frenchName.firstName;
 }
-console.log(getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' }));
+console.log('Value of getFirstName: ' + getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' }));
 
 function getLastElement(array) {
   var hankHill = array;
   return hankHill[3];
 }
-console.log(getLastElement(['propane', 'and', 'propane', 'accessories']));
+console.log('Value of getLastElement: ' + getLastElement(['propane', 'and', 'propane', 'accessories']));
