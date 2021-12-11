3f8636e39cbaf458f8fac3f91ab6292da6c5c70a
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-10 21:04:28 -0800

    attempted string-manipulation-1.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -7,8 +7,14 @@
 // Once loop has been completed, it will return the value of the array.
 function getWords(string) {
   var wordArray = [];
-  var newWord = '';
-  newWord = string.split(' ');
-  wordArray += newWord;
+  // needs modification
+  // if (string === ' ') {
+  // wordArray.split(string.length);
+  if (string === ' ') {
+    wordArray.push(string.slice(0));
+  }
+  if (string !== '') {
+    wordArray.push(string);
+  }
   return wordArray;
 }
