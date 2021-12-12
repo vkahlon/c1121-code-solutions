622e82183ad68f4bd444c570dba7426b5fd65c64
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-11 18:31:44 -0800

    Completed string-manipulation-1 exercise.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,20 +1,17 @@
 /* exported getWords */
 // For this procedure, we will take a string and place it into an array
 // -These Strings can be multiple words, therefore the split process will be employed.
-// A iteration will run and will run the amount of words that are in the potential string.
-// Need storage for the array before this iteration.
-// In each iteration, it will split the word, depending where it is in the index, and then push it into the storage, that was created earlier.
-// Once loop has been completed, it will return the value of the array.
+// To take account for an empty string, a stict equality of comparsion of the string and empty string will be compared
+// if it matches, the string will be split and returned.
+// In any other case, the string will be split whenever a space occurs, will be stored into an array via method
+// the array will be returned via the words storage space.
 function getWords(string) {
-  var wordArray = [];
-  // needs modification
-  // if (string === ' ') {
-  // wordArray.split(string.length);
-  if (string === ' ') {
-    wordArray.push(string.slice(0));
+  var words = '';
+  if (string === '') {
+    words = string.split('');
+    return words;
+  } else {
+    words = string.split(' ');
+    return words;
   }
-  if (string !== '') {
-    wordArray.push(string);
-  }
-  return wordArray;
 }
