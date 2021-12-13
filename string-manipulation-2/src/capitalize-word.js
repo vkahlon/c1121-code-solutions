a25d1acbb4bd8d6c365fb6c44b274b4556010829
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-13 11:27:14 -0800

    attempted string-manipulation-2.

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,16 @@
 /* exported capitalizeWord */
+// In this process, we will be taking a string and fixing its capitalization
+// need to allocate storage space for the new fixed string
+// In order to fix the capitalization, we will force every string at character 0 to be uppercased.
+// For the second part of the capitalization, we will force the remaining cases to be lower, with exception to JavaScript
+// We will filter out javascript by employing an comparison check with it's starting character j or J
+// Once the process is complete, it will return the value of the process.
+function capitalizeWord(word) {
+  var newWordOne = '';
+  var newWordTwo = '';
+  if ((word.charAt(0) === 'j') ||
+    (word.charAt(0) === 'J')) {
+    newWordOne = word;
+    return newWordOne;
+  } return newWordTwo;
+}
--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -3,9 +3,10 @@
 //  it returns a value of the string with the omitted characters and an ellipsis.
 // A new word will be allocated in storage space in order to preserve the original string.
 // This process will utilize the slice method and its argument value will be the value of length.
+// This shortened word will be assigned to the newWord storage space.
 // it will return the value of the shortened string concaneted with three dots.
 function truncate(length, string) {
   var newWord = string;
-  newWord.slice(length);
+  newWord = newWord.slice(0, length);
   return newWord + '...';
 }
