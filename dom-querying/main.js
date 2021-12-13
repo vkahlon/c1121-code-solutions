1737c203cfaa168402684618c8e61ba58232af5d
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-13 10:23:32 -0800

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,14 @@
+var h1Element = document.querySelector('div > h1');
+console.log(h1Element);
+
+var idElement = document.querySelector('#explanation');
+console.log(idElement);
+
+var classElement = document.querySelector('.hint');
+console.log(classElement);
+
+var allPElement = document.querySelector('p');
+console.log(allPElement);
+
+var classTwoElement = document.querySelector('.example-link');
+console.log(classTwoElement);
