27244776372907e0736638bf71a5426e175b41a9
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-14 15:54:26 -0800

    Completed javascript-light-bulb exercise.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,14 +1,18 @@
 var clickCount = 0;
+var clickStatus = false;
 function letThereBeLight(event) {
   clickCount++;
   if (clickCount % 2 === 1) {
+    clickStatus = true;
+  } else {
+    clickStatus = false;
+  }
+  if (clickStatus === true) {
     $lightButton.className = 'button-on';
     $lightBackground.className = 'row background-color on';
-
   } else {
     $lightButton.className = 'button-off';
     $lightBackground.className = 'row background-color off';
-
   }
 }
 var $lightButton = document.querySelector('.button-off');
