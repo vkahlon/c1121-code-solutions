738ba8ed605f489d6c5aea8c476d258b3432a4cf
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-15 13:26:19 -0800

    Completed javascript-modal.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,21 +1,21 @@
 var isModalOpen = false;
 function callMod(event) {
   if (isModalOpen === false) {
-    $convertModa.className = 'row modal-window';
+    $convertModal.className = 'row modal-window';
     $convertOverlay.className = 'overlay active';
     isModalOpen = true;
   } else {
-    $convertModa.className = 'row modal-window-hidden';
+    $convertModal.className = 'row modal-window-hidden';
     $convertOverlay.className = 'overlay inactive';
     isModalOpen = false;
   }
 }
 
-var $selectModa = document.querySelector('button.open-moda');
-$selectModa.addEventListener('click', callMod);
+var $selectModal = document.querySelector('button.open-modal');
+$selectModal.addEventListener('click', callMod);
 
-var $closeModa = document.querySelector('button.no-moda');
-$closeModa.addEventListener('click', callMod);
+var $closeModal = document.querySelector('button.no-modal');
+$closeModal.addEventListener('click', callMod);
 
-var $convertModa = document.querySelector('.row.modal-window-hidden');
+var $convertModal = document.querySelector('.row.modal-window-hidden');
 var $convertOverlay = document.querySelector('.overlay.inactive');
