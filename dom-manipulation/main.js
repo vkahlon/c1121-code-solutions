21ac8fe94201745f6ca4c8cd0e79547e81ee8c0d
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-14 20:48:29 -0800

    Completed dom-manipulation exercise.

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -5,25 +5,20 @@ function trackCount(event) {
   $clickCount.textContent = totalCount;
   if (buttonCount < 4) {
     $hotButton.className = 'hot-button cold';
-  }
-  if ((buttonCount >= 4) && (buttonCount < 7)) {
+  } else if (buttonCount < 7) {
     $hotButton.className = 'hot-button cool';
-  }
-  if ((buttonCount >= 7) && (buttonCount < 10)) {
+  } else if (buttonCount < 10) {
     $hotButton.className = 'hot-button tepid';
-  }
-  if ((buttonCount >= 10) && (buttonCount < 13)) {
+  } else if (buttonCount < 13) {
     $hotButton.className = 'hot-button warm';
-  }
-  if ((buttonCount >= 13) && (buttonCount < 16)) {
+  } else if (buttonCount < 16) {
     $hotButton.className = 'hot-button hot';
-  }
-  if (buttonCount >= 16) {
+  } else if (buttonCount >= 16) {
     $hotButton.className = 'hot-button nuclear';
   }
   return totalCount;
 }
 var $clickCount = document.querySelector('.click-count');
-$clickCount.addEventListener('click', trackCount);
+
 var $hotButton = document.querySelector('.hot-button');
 $hotButton.addEventListener('click', trackCount);
