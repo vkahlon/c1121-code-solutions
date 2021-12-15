1323ffd43b48d0ba00e48b6aa7e2d2a7e0c46c9e
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-15 09:57:00 -0800

    Completed string-manipulation-2 exercise.

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,6 @@
 /* exported ransomCase */
+// lowercase the entire string and assign it to a new storage space
+function ransomCase(string) {
+  var lowerString = string.toLowerCase();
+  return lowerString;
+}
