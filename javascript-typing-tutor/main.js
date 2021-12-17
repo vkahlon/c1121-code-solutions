3876720d72c038f96724de20c61fa17a2e7cd91e
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-17 15:20:03 -0800

    attempted javascript-typing-tutor exercise.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,7 +1,7 @@
 function recordKey(event) {
 }
 
-var $letterSelectorAll = document.querySelectorAll('span');
+var $letterSelectorAll = document.querySelectorAll('body');
 for (var i = 0; i < $letterSelectorAll.length; i++) {
   $letterSelectorAll[i].addEventListener('keydown', recordKey);
 }
