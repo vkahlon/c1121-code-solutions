242d0ab3f6ebcddbec1484e28980bc2b578e60a5
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-16 17:35:10 -0800

    Completed javascript-view-swapping exercise.

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,29 @@
+function callBack(event) {
+  var targetMatch = event.target;
+
+  if (targetMatch.matches('.tab')) {
+    for (var i = 0; i < $tabCollection.length; i++) {
+      if ($tabCollection[i] === targetMatch) {
+        $tabCollection[i].className = 'tab active';
+      } else {
+        $tabCollection[i].className = 'tab';
+      }
+    }
+  }
+  if (targetMatch.matches('.tab')) {
+    var dataViewValue = targetMatch.getAttribute('data-view');
+    for (i = 0; i < $viewCollection.length; i++) {
+      if ($viewCollection[i].getAttribute('data-view') === dataViewValue) {
+        $viewCollection[i].className = 'view';
+      } else {
+        $viewCollection[i].className = 'view hidden';
+      }
+    }
+  }
+}
+
+var $tabContainer = document.querySelector('.tab-container');
+var $tabCollection = document.querySelectorAll('.tab');
+var $viewCollection = document.querySelectorAll('.view');
+
+$tabContainer.addEventListener('click', callBack);
