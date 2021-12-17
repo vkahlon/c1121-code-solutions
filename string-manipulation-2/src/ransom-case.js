de21cb0e23d7a269d6063570354f71e1a199ac46
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-16 17:57:46 -0800

    Completed string-manipulation-2

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,10 +1,13 @@
 /* exported ransomCase */
 // lowercase the entire string and assign it to a new storage space
+// implement a for loop, have it start at index one as it's every other character
+// have it run until it reaches the lowerstring length and increment by two since we're using every other two letters.
+// have the index begin at 0, ends at index, uppercase the index, and again start index, end at index by 1
 function ransomCase(string) {
   var lowerString = string.toLowerCase();
   for (var i = 1; i < lowerString.length; i += 2) {
-    var upperLetter = lowerString.charAt(i);
-    var newString = upperLetter.toUpperCase() + lowerString.slice(i + 1);
+    lowerString = lowerString.substr(0, i) + lowerString[i].toUpperCase() + lowerString.substr(i + 1);
+    var newString = lowerString;
   }
   return newString;
 }
