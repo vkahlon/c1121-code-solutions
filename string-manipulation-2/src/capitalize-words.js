a567d1bfab1927a31a74df45d7e3830f335e5fa5
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-18 17:28:07 -0800

    Completed String-manipulation-2 exercise.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,18 @@
 /* exported capitalizeWords */
+// lowercase the string first
+// split the string into an array thorugh the split method, with the intent with utilizing the join method later.
+// utilize iteration, run until out of words, in the iteration, uppercase each index and restore the rest of the characters through substring method.
+// utilize the join method to make the array back into a string
+// return the string.
+function capitalizeWords(string) {
+  var lowerCasedString = string.toLowerCase();
+  var newArrayWord = lowerCasedString.split(' ');
+
+  for (var i = 0; i < newArrayWord.length; i++) {
+    var newUpperCaseLetter = newArrayWord[i][0].toUpperCase();
+    var restOfWord = newArrayWord[i].substring(1);
+    var combinedWord = newUpperCaseLetter + restOfWord;
+    newArrayWord[i] = combinedWord;
+  }
+  return newArrayWord.join(' ');
+}
--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,9 @@
 /* exported lastChars */
+// implement a new process with the inputs of its length and the phrase that will be used
+// slice the string to the length of it requested, however, make it a negative number for precision reasons, and assign it
+// allocate storage space for the cut phrase
+// return the cut phrase.
+function lastChars(length, string) {
+  var newPhrase = string.slice(-length);
+  return newPhrase;
+}
