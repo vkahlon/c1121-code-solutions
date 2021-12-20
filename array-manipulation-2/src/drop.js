f6b55f1815da18b4bbebdcc8692fff26309f5ff9
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-20 09:56:58 -0800

    Completed array-manipulation-2 exercise.

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,9 +1,16 @@
 /* exported drop */
+// For this process, it will take in a list and a number input
+// allocate storage space for a new list
+// allocate storage space for the number that was inputted
+// create an iteration, begin at zero, have it run to the list length subtracted by the amount of count from earlier input
+// Set the new list with the previous inputted lists value, and incrememnt the number.
+// Once the iteration has been completed, return the new list.
 function drop(array, count) {
   var newArray = [];
-
-  for (var i = count; i < array.length; i++) {
-    newArray[i] = array[i];
+  var dedicatedCount = count;
+  for (var i = 0; i < array.length - dedicatedCount; i++) {
+    newArray[i] = array[count];
+    count++;
   }
   return newArray;
 }
