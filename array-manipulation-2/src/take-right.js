b1b279400417307876b10204e88593b313eac868
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-21 09:43:19 -0800

    attempted array manip 2.

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,9 @@
 /* exported takeRight */
+function takeRight(array, count) {
+  var newArray = array;
+  var selection = count;
+  for (var i = 0; i < selection; i++) {
+    newArray[i] = (array[count]);
+  }
+  return newArray;
+}
