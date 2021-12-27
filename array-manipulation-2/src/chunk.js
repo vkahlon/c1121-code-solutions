f8b6151ace5b2fb9156e6df0b60652429ae96fe0
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-27 11:05:39 -0800

    attempted array-manipulation-2 exercise.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,6 +1,9 @@
 /* exported chunk */
 function chunk(array, size) {
+  var theArray = [];
   var newArray = array.slice(0, size);
-  // var newArray2 = array.slice(size);
-  return newArray;
+  var newArray2 = array.slice(size);
+  theArray[0] = newArray;
+  theArray[1] = newArray2;
+  return theArray;
 }
