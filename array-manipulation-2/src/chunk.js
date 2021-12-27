fb470aa695853b3c3e8ccc695abdccb6d2302a0b
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-26 18:57:13 -0800

    attempted level 2 challenges.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,6 @@
 /* exported chunk */
+function chunk(array, size) {
+  var newArray = array.slice(0, size);
+  // var newArray2 = array.slice(size);
+  return newArray;
+}
--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,9 +1,9 @@
 /* exported takeRight */
+// Define a new process and take in a list and a number
+// assign storage space for the shallow copy of the list.
+// utilize the slice method to take a sub section of an array.
+// return the storage space
 function takeRight(array, count) {
-  var newArray = array;
-  var selection = count;
-  for (var i = 0; i < selection; i++) {
-    newArray[i] = (array[count]);
-  }
+  var newArray = array.slice(-count);
   return newArray;
 }
