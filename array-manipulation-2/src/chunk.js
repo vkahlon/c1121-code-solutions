cb02fee3c69d393d48bbe3f803063871e33f536c
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-27 17:12:21 -0800

    Completed Array Manipulation 2.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,12 +1,15 @@
 /* exported chunk */
+// declare a process which takes in a list and the amount of times it gets split
+// declare an empty list and allocate it to storage space
+// declare an iteration, have it run the length of the list, increment it by split size
+// slice list, starting value of count, and end it by count + split size and assign storage space for a list
+// push the in process list storage space into the main list that was declared earlier.
+// after the iteration is complete return the main list
 function chunk(array, size) {
   var chunkyArray = [];
-  var theArray = [];
-  if (2 % size === 0) {
-    chunkyArray = [array.slice(0, size)];
-    for (var i = size; i < array.length; i++) {
-      theArray.push(array[i]);
-    }
-  } return chunkyArray;
-
+  for (var i = 0; i < array.length; i += size) {
+    var innerArray = array.slice(i, i + size);
+    chunkyArray.push(innerArray);
+  }
+  return chunkyArray;
 }
