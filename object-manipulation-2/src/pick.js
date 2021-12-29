0db30aeb90b5378c0dc1d3c4d854d51d8849217f
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-28 22:24:49 -0800

    attempted object-manipulation-2 exercise.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,13 +1,10 @@
 /* exported pick */
-function pick(source, keys) {
-  var newObject = {};
-  for (var captKey in source) {
-    if (keys.includes(captKey)) {
-      newObject[captKey] = source[captKey];
-    }
-  }
-  return newObject;
-  // for (var i = 0; i < keys.length; i++) {
-  //   newObject = source;
-  // }
-}
+// function pick(source, keys) {
+//   var newObject = {};
+//   for (var i = 0; i < source.length; i++) {
+//     if (source === keys[i]) {
+//       newObject.keys[i] = keys[i];
+//     }
+//   }
+//   return newObject;
+// }
