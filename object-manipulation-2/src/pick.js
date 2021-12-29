d7a0c8d453bc3687e462a7a7ed8cd2204206e0eb
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-29 11:59:37 -0800

    Attempted object-manipulation-2.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,10 +1,16 @@
 /* exported pick */
-// function pick(source, keys) {
-//   var newObject = {};
-//   for (var i = 0; i < source.length; i++) {
-//     if (source === keys[i]) {
-//       newObject.keys[i] = keys[i];
-//     }
-//   }
-//   return newObject;
-// }
+// Edge case: falsy values exist, includes will not work.
+// declare a process that takes in an entity and list
+// allocate storage space for an entity
+//
+function pick(source, keys) {
+  var newObject = {};
+  for (var captKey in source) {
+    for (var i = 0; i < keys.length; i++) {
+      if (keys[i] === captKey) {
+        newObject[captKey] = source[captKey];
+      }
+    }
+  }
+  return newObject;
+}
