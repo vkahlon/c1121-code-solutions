15f9a0d42deb1e9c11448adc3ee80fed13387058
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-29 21:16:42 -0800

    Completed object-manipulation-2 exercise.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -2,14 +2,15 @@
 // Edge case: falsy values exist, includes will not work.
 // declare a process that takes in an entity and list
 // allocate storage space for an entity
-//
+// declare an interation, have it run at the input list length
+// check to see if the input entity at input list at index is inside, make sure it isnt undefined due to edge case
+// if so, grab the input entity at input list at index, assign it to its property of the entity object
+// return the storage space entity
 function pick(source, keys) {
   var newObject = {};
-  for (var captKey in source) {
-    for (var i = 0; i < keys.length; i++) {
-      if (keys[i] === captKey) {
-        newObject[captKey] = source[captKey];
-      }
+  for (var i = 0; i < keys.length; i++) {
+    if (source[keys[i]] !== undefined) {
+      newObject[keys[i]] = source[keys[i]];
     }
   }
   return newObject;
