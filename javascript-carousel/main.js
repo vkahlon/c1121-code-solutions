db55bba0b817ea686b0c9e97b768e16fa4175eae
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-29 17:54:03 -0800

    Completed javascript-carousel.

---
+++ javascript-carousel/main.js
@@ -0,0 +1,71 @@
+var imgSource = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
+var imgSelector = document.querySelector('.img-wrapper');
+var bullets = document.querySelectorAll('.fa-circle');
+
+// var timer =
+setInterval(swapItem, 3000);
+var theCount = 0;
+function swapItem() {
+  theCount++;
+  if (theCount === 5) {
+    theCount = 0;
+    bullets[4].className = 'far fa-circle';
+    bullets[theCount].className = 'fas fa-circle';
+    imgSelector.setAttribute('src', imgSource[theCount]);
+    return theCount;
+  }
+  bullets[theCount - 1].className = 'far fa-circle';
+  bullets[theCount].className = 'fas fa-circle';
+  imgSelector.setAttribute('src', imgSource[theCount]);
+  return theCount;
+}
+
+var clickAButton = document.querySelector('.bullets');
+clickAButton.addEventListener('click', clickButton);
+function clickButton(event) {
+  for (var i = 0; i < bullets.length; i++) {
+    if (bullets[i] === event.target) {
+      bullets[theCount].className = 'far fa-circle';
+      bullets[i].className = 'fas fa-circle';
+      imgSelector.setAttribute('src', imgSource[i]);
+      theCount = i;
+      return theCount;
+    } else {
+      bullets[i].className = 'far fa-circle';
+    }
+  }
+}
+var clickRight = document.querySelector('.fa-angle-right');
+clickRight.addEventListener('click', moveRight);
+var clickLeft = document.querySelector('.fa-angle-left');
+clickLeft.addEventListener('click', moveLeft);
+
+function moveRight(event) {
+  theCount++;
+  if (theCount === 5) {
+    theCount = 0;
+    bullets[4].className = 'far fa-circle';
+    bullets[theCount].className = 'fas fa-circle';
+    imgSelector.setAttribute('src', imgSource[theCount]);
+    return theCount;
+  }
+  bullets[theCount - 1].className = 'far fa-circle';
+  bullets[theCount].className = 'fas fa-circle';
+  imgSelector.setAttribute('src', imgSource[theCount]);
+  return theCount;
+}
+
+function moveLeft(event) {
+  theCount--;
+  if (theCount === -1) {
+    theCount = 4;
+    bullets[0].className = 'far fa-circle';
+    bullets[theCount].className = 'fas fa-circle';
+    imgSelector.setAttribute('src', imgSource[theCount]);
+    return theCount;
+  }
+  bullets[theCount + 1].className = 'far fa-circle';
+  bullets[theCount].className = 'fas fa-circle';
+  imgSelector.setAttribute('src', imgSource[theCount]);
+  return theCount;
+}
