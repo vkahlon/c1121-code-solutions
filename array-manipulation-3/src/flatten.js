06c0fc78ac0f7dd69a8e89abb6bfb7e1e75fbfc2
Vickram Kahlon <vickram.kahlon@gmail.com>
2022-01-11 14:49:38 -0800

    Completed flatten.js portion.

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,4 +1,8 @@
 /* exported flatten */
+// declare a process with a list as a parameter
+// Utilize the concat method of an empty array, which takes a single argument, the spread operator with the input array, assign the result of the expression to the variable flatArray.
+// return the flattenArray storage space
 function flatten(array) {
-
+  var flattenedArray = [].concat(...array);
+  return flattenedArray;
 }
