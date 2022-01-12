26aebe5afe86450ca5f7ba038b5a84086596d0c2
Vickram Kahlon <vickram.kahlon@gmail.com>
2022-01-11 17:45:23 -0800

    attempted step 6.

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,6 @@
 /* exported difference */
+// declare a process with two parameters, both being lists
+// assign storage space to a new list, that is empty
+// utilize the filter method of the first object that calls an anonymous function
+// utilize the filter method of the second object that calls an anonymous function
+// In secondArraySort
