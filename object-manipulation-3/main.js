ba78c7827a2d0651231802dbffeed0b534aa3e05
Vickram Kahlon <vickram.kahlon@gmail.com>
2022-01-11 21:02:09 -0800

    attempted a portion of the card game.

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,48 @@
+// console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// var club = [];
+// var clubsDeck = createDeck('clubs', club);
+// var spades = [];
+// var spadesDeck = createDeck('spades', spades);
+// var the26One = clubsDeck.concat(spadesDeck);
+// var heart = [];
+// var heartsDeck = createDeck('hearts', heart);
+// var diamond = [];
+// var diamondsDeck = createDeck('diamonds', diamond);
+// var the26Two = heartsDeck.concat(diamondsDeck);
+// var fullDeck = the26One.concat(the26Two);
+
+// var hand1 = fullDeck[Math.floor(fullDeck.length * Math.random())];
+// var hand2 = fullDeck[Math.floor(fullDeck.length * Math.random())];
+// var hand3 = fullDeck[Math.floor(fullDeck.length * Math.random())];
+// var hand4 = fullDeck[Math.floor(fullDeck.length * Math.random())];
+// function createDeck(suit, list) {
+//   for (var i = 2; i < 11; i++) {
+//     var newCard = { rank: i, suit: suit };
+//     list.push(newCard);
+//   }
+//   var newJack = { rank: 'Jack', suit: suit };
+//   list.push(newJack);
+//   var newQueen = { rank: 'Queen', suit: suit };
+//   list.push(newQueen);
+//   var newKing = { rank: 'King', suit: suit };
+//   list.push(newKing);
+//   var newAce = { rank: 'Ace', suit: suit };
+//   list.push(newAce);
+//   return list;
+// }
+// var player1 = {
+//   name: 'Skip Bayless',
+//   hand: hand1
+// };
+// var player2 = {
+//   name: 'Shannon Sharpe',
+//   hand: hand2
+// };
+// var player3 = {
+//   name: 'Stephen A',
+//   hand: hand3
+// };
+// var player4 = {
+//   name: 'Ray L',
+//   hand: hand4
+// };
