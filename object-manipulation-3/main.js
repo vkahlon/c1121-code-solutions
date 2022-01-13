3e1733d68d9320dcfa8bfa839b7e993321ccdf13
Vickram Kahlon <vickram.kahlon@gmail.com>
2022-01-13 14:03:49 -0800

    Completed object-manipulation-3.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,94 +1,92 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-var club = [];
-var clubsDeck = createDeck('clubs', club);
-var spades = [];
-var spadesDeck = createDeck('spades', spades);
-var the26One = clubsDeck.concat(spadesDeck);
-var heart = [];
-var heartsDeck = createDeck('hearts', heart);
-var diamond = [];
-var diamondsDeck = createDeck('diamonds', diamond);
-var the26Two = heartsDeck.concat(diamondsDeck);
-var fullDeck = the26One.concat(the26Two);
-var shuffledCards = _.shuffle(fullDeck);
-var hand1 = [shuffledCards[0], shuffledCards[1]];
-var hand2 = [shuffledCards[2], shuffledCards[3]];
-var hand3 = [shuffledCards[4], shuffledCards[5]];
-var hand4 = [shuffledCards[6], shuffledCards[7]];
+var playerList = [
+  {
+    name: 'Skip Bayless',
+    hand: null
+  },
+  {
+    name: 'Shannon Sharpe',
+    hand: null
+  },
+  {
+    name: 'Stephen A',
+    hand: null
+  },
+  {
+    name: 'Ray L',
+    hand: null
+  }];
+startsGame(playerList);
 
-function createDeck(suit, list) {
-  for (var i = 2; i < 11; i++) {
-    var newCard = { rank: i, suit: suit };
-    list.push(newCard);
-  }
-  var newJack = { rank: 'Jack', suit: suit };
-  list.push(newJack);
-  var newQueen = { rank: 'Queen', suit: suit };
-  list.push(newQueen);
-  var newKing = { rank: 'King', suit: suit };
-  list.push(newKing);
-  var newAce = { rank: 'Ace', suit: suit };
-  list.push(newAce);
-  return list;
-}
-var player1 = {
-  name: 'Skip Bayless',
-  hand: hand1
-};
-var player2 = {
-  name: 'Shannon Sharpe',
-  hand: hand2
-};
-var player3 = {
-  name: 'Stephen A',
-  hand: hand3
-};
-var player4 = {
-  name: 'Ray L',
-  hand: hand4
-};
-var ranks = grabRankings(player1, player2, player3, player4);
-function grabRankings(player1, player2, player3, player4) {
-  var theTable = [];
-  var getPlayer1Rank1 = getARank(player1.hand[0].rank);
-  var getPlayer1Rank2 = getARank(player1.hand[1].rank);
-  var getPlayer1TotalRank = getPlayer1Rank1 + getPlayer1Rank2;
+function startsGame(playerList) {
+  var emptyDeck = [];
+  var clubsDeck = createDeck('clubs', emptyDeck);
+  var spadesDeck = createDeck('spades', emptyDeck);
+  var the26One = clubsDeck.concat(spadesDeck);
+  var heartsDeck = createDeck('hearts', emptyDeck);
+  var diamondsDeck = createDeck('diamonds', emptyDeck);
+  var the26Two = heartsDeck.concat(diamondsDeck);
+  var fullDeck = the26One.concat(the26Two);
+  var shuffledCards = _.shuffle(fullDeck);
 
-  var getPlayer2Rank1 = getARank(player2.hand[0].rank);
-  var getPlayer2Rank2 = getARank(player2.hand[1].rank);
-  var getPlayer2TotalRank = getPlayer2Rank1 + getPlayer2Rank2;
-
-  var getPlayer3Rank1 = getARank(player3.hand[0].rank);
-  var getPlayer3Rank2 = getARank(player3.hand[1].rank);
-  var getPlayer3TotalRank = getPlayer3Rank1 + getPlayer3Rank2;
-
-  var getPlayer4Rank1 = getARank(player4.hand[0].rank);
-  var getPlayer4Rank2 = getARank(player4.hand[1].rank);
-  var getPlayer4TotalRank = getPlayer4Rank1 + getPlayer4Rank2;
-
-  var player1Rank = { name: 'Skip Bayless', points: getPlayer1TotalRank };
-  var player2Rank = { name: 'Shannon Sharpe', points: getPlayer2TotalRank };
-  var player3Rank = { name: 'Stephen A', points: getPlayer3TotalRank };
-  var player4Rank = { name: 'Ray L', points: getPlayer4TotalRank };
-  theTable.push(player1Rank);
-  theTable.push(player2Rank);
-  theTable.push(player3Rank);
-  theTable.push(player4Rank);
-  function getARank(card) {
-    if (card === 'Jack' || card === 'Queen' || card === 'King') {
-      card = 10;
-      return card;
-    } else if (card === 'Ace') {
-      card = 11;
-      return card;
-    } else {
-      return card;
+  function createDeck(suit, list) {
+    for (var i = 2; i < 11; i++) {
+      var newCard = { rank: i, suit: suit };
+      list.push(newCard);
     }
+    var newJack = { rank: 'Jack', suit: suit };
+    list.push(newJack);
+    var newQueen = { rank: 'Queen', suit: suit };
+    list.push(newQueen);
+    var newKing = { rank: 'King', suit: suit };
+    list.push(newKing);
+    var newAce = { rank: 'Ace', suit: suit };
+    list.push(newAce);
+    return list;
   }
-  return theTable;
+  for (var i = 0; i < playerList.length; i++) {
+    playerList[i].hand = [shuffledCards[i], shuffledCards[i + 10]];
+  }
+  var getRank = grabRankings(playerList);
+  function grabRankings(playerList) {
+    var theTable = [];
+    for (var i = 0; i < playerList.length; i++) {
+      var getPlayerName = playerList[i].name;
+      var getPlayerRank1 = getARank(playerList[i].hand[0].rank);
+      var getPlayerRank2 = getARank(playerList[i].hand[1].rank);
+      var getPlayer1TotalRank = getPlayerRank1 + getPlayerRank2;
+      var playerStats = { name: getPlayerName, points: getPlayer1TotalRank };
+      theTable.push(playerStats);
+    }
+
+    function getARank(card) {
+      if (card === 'Jack' || card === 'Queen' || card === 'King') {
+        card = 10;
+        return card;
+      } else if (card === 'Ace') {
+        card = 11;
+        return card;
+      } else {
+        return card;
+      }
+    }
+    return theTable;
+  }
+  var grabWinnerList = _.sortBy(getRank, ['points']);
+  var getPlayerCount = grabWinnerList.length - 1;
+  if (grabWinnerList[getPlayerCount].points === grabWinnerList[getPlayerCount - 1].points) {
+    console.log('TIE BREAKER!');
+    var newGame = [];
+    var grabTieBreakerPlayer1 = { name: grabWinnerList[2].name, hand: null };
+    var grabTieBreakerPlayer2 = { name: grabWinnerList[3].name, hand: null };
+    newGame.push(grabTieBreakerPlayer1);
+    newGame.push(grabTieBreakerPlayer2);
+    var startNewRound = startsGame(newGame);
+    console.log('The winner is ' + startNewRound[1].name + ' who had ' + startNewRound[1].points + ' points.');
+    console.log('The runner-up is ' + startNewRound[0].name + ' who had ' + startNewRound[0].points + ' points.');
+  } else if (grabWinnerList.length > 2) {
+    console.log('The winner is ' + grabWinnerList[getPlayerCount].name + ' who had ' + grabWinnerList[getPlayerCount - 1].points + ' points.');
+    console.log('The runner-up is ' + grabWinnerList[getPlayerCount - 1].name + ' who had ' + grabWinnerList[getPlayerCount - 2].points + ' points.');
+  }
+  return grabWinnerList;
 }
-var grabWinnerList = _.sortBy(ranks, ['points']);
-console.log('The winner is ' + grabWinnerList[3].name + ' who had ' + grabWinnerList[3].points + ' points.');
-console.log('Second place is ' + grabWinnerList[2].name + ' who had ' + grabWinnerList[2].points + ' points.');
-console.log('Third place is ' + grabWinnerList[1].name + ' who had ' + grabWinnerList[1].points + ' points.');
-console.log('Fourth place is ' + grabWinnerList[0].name + ' who had ' + grabWinnerList[0].points + ' points.');
