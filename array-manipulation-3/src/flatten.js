7a3b6c913a43f93f789995da4a3f469ef49041d6
Vickram Kahlon <vickram.kahlon@gmail.com>
2022-01-13 14:18:08 -0800

    Completed array-manipulation-3.

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,4 +1,5 @@
 /* exported flatten */
+// adding comment here so i can re-commit
 // no spread operator, beginners aren't allowed to use it
 // declare a process with a list as a parameter
 // Utilize the apply method of the concat property of the array list, which takes a two arguments, an empty array literal, and the input list, assign the result of the expression to the variable flatArray.
