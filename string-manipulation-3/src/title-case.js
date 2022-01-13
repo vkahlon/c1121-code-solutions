24cf48dc5598efe32d7622256af11d2c41a76865
Vickram Kahlon <vickram.kahlon@gmail.com>
2022-01-13 11:53:03 -0800

    Patched string-manip-3.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -9,8 +9,8 @@
 // if the word isnt in the lowercased list, uppercase the first letter, and use the substring method to reattach the word
 // if the letter contains a colon, upperCase the next word in the list
 // if the letter contains a dash, uppercase the next letter
-// EDGE CASE: if the first letter, starts with a J, check to see if it contains a colon, if it does, assign the word as JavaScript: if not assign the word as JavaScript
-// EDGE CASE: if the first letter starts with A, and the second letter with p, assign the word as API
+// EDGE CASE: if the word is equal to 'Javascript:', assign the word as JavaScript:
+// EDGE CASE: if the word is equal to 'Javascript', assign the word as JavaScript
 // combine the list into one through the join method with a space, and return it.
 
 function titleCase(title) {
@@ -35,14 +35,13 @@ function titleCase(title) {
       var newWord = words[i].substring(0, location + 1) + upperCaseLetter + words[i].substring(location + 2, words[i].length);
       words[i] = newWord;
     }
-    if (words[i].charAt(0) === 'J') {
-      if (words[i].indexOf(':') !== -1) {
-        words[i] = 'JavaScript:';
-      } else {
-        words[i] = 'JavaScript';
-      }
+    if (words[i] === 'Javascript') {
+      words[i] = 'JavaScript';
     }
-    if ((words[i].charAt(0) === 'A') && (words[i].charAt(1) === 'p')) {
+    if (words[i] === 'Javascript:') {
+      words[i] = 'JavaScript:';
+    }
+    if ((words[i] === 'Api')) {
       words[i] = 'API';
     }
   }
