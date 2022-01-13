c1a9a21cdd4fe47867f1948337a6edeaa3fcf8b7
Vickram Kahlon <vickram.kahlon@gmail.com>
2022-01-13 11:12:04 -0800

    Completed string-manipulation-3 exercise.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,24 +2,45 @@
 // declare a process with an input of a string
 // grab the input title, lower case it, and assign it to storage space.
 // have a list of words that you want to keep lowercased and assign it to storage space.
-// split all the words into an array and assign it to storage space.
-// have an empty string and assign it storage space
+// split all the words into a list and assign it to storage space.
+// Since the first letter will always be uppercased, uppercase the first letter, and reattach the rest of the word and assign it to the first word.
+// EDGE CASE: if the first letter is an uppercased J, assign that word as JavaScript:, assign the second word with an uppercase T, in the future and other instances, you will factor in other colons
+// declare an iteration that starts at the second word of the list and goes till the last word
+// if the word isnt in the lowercased list, uppercase the first letter, and use the substring method to reattach the word
+// if the letter contains a colon, upperCase the next word in the list
+// if the letter contains a dash, uppercase the next letter
+// EDGE CASE: if the first letter, starts with a J, check to see if it contains a colon, if it does, assign the word as JavaScript: if not assign the word as JavaScript
+// EDGE CASE: if the first letter starts with A, and the second letter with p, assign the word as API
+// combine the list into one through the join method with a space, and return it.
 
 function titleCase(title) {
   var lowerCased = title.toLowerCase();
   var lowerCasedList = ['and', 'or', 'nor', 'but', 'a', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to', 'the'];
   var words = lowerCased.split(' ');
-  words[0] = words[0].charAt(0).toUpperCase() + words[0].substring(1).toLowerCase();
-
+  words[0] = words[0].charAt(0).toUpperCase() + words[0].substring(1);
+  if (words[0].charAt(0) === 'J') {
+    words[0] = 'JavaScript:';
+    words[1] = 'The';
+  }
   for (var i = 1; i < words.length; i++) {
     if (lowerCasedList.indexOf(words[i]) === -1) {
-      words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1).toLowerCase();
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
+    }
+    if (words[i].indexOf(':') !== -1) {
+      words[i + 1] = words[i + 1].charAt(0).toUpperCase() + words[i + 1].substring(1);
+    }
+    if (words[i].indexOf('-') !== -1) {
+      var location = words[i].indexOf('-');
+      var upperCaseLetter = words[i].charAt(location + 1).toUpperCase();
+      var newWord = words[i].substring(0, location + 1) + upperCaseLetter + words[i].substring(location + 2, words[i].length);
+      words[i] = newWord;
     }
     if (words[i].charAt(0) === 'J') {
-      words[i] = 'JavaScript';
-    }
-    if ((words[i].charAt(0) === 'J') && (words[i].charAt(10) === ':')) {
-      words[i] = 'JavaScript:';
+      if (words[i].indexOf(':') !== -1) {
+        words[i] = 'JavaScript:';
+      } else {
+        words[i] = 'JavaScript';
+      }
     }
     if ((words[i].charAt(0) === 'A') && (words[i].charAt(1) === 'p')) {
       words[i] = 'API';
@@ -27,41 +48,3 @@ function titleCase(title) {
   }
   return words.join(' ');
 }
-
-// Spaghetti below
-// function titleCase(title) {
-//   var lowerCasedList = ['and', 'or', 'nor', 'and', 'but', 'a', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
-//   var words = title.split(' ');
-//   var newTitle = '';
-//   for (var i = 0; i < words.length - 1; i++) {
-//     if ((words[i].charAt(0) === 'j') ||
-//       (words[i].charAt(0) === 'J')) {
-//       var newWord = 'JavaScript ';
-//       newTitle += newWord;
-//     }
-//     if (lowerCasedList.indexOf(words[i]) === -1) {
-//       var newUpperCase = words[i].charAt(0);
-//       newUpperCase = newUpperCase.toUpperCase();
-
-//       var restOfWord = words[i].substring(1);
-//       restOfWord = restOfWord.toLocaleLowerCase();
-//       var newWord = newUpperCase + restOfWord;
-//       newTitle += newWord + ' ';
-//     } else if (lowerCasedList.indexOf(words[i]) !== -1) {
-//       newTitle += words[i] + ' ';
-//     }
-//   }
-//   var lastWord = words.length - 1;
-//   if (lowerCasedList.indexOf(lastWord) === -1) {
-//     var newLastUpperCase = words[lastWord].charAt(0);
-//     newLastUpperCase = newLastUpperCase.toUpperCase();
-
-//     var restOfLastWord = words[lastWord].substring(1);
-//     restOfLastWord = restOfLastWord.toLocaleLowerCase();
-//     var theLastWord = newLastUpperCase + restOfLastWord;
-//     newTitle += theLastWord;
-//   } else if (lowerCasedList.indexOf(words[lastWord]) !== -1) {
-//     newTitle += theLastWord;
-//   }
-//   return newTitle;
-// }
