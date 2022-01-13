3c4510cff86f845a181df0fd9154b33cb2c24b5d
Vickram Kahlon <vickram.kahlon@gmail.com>
2022-01-12 17:47:41 -0800

    attempted string-manipulation-3.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,8 @@
 /* exported titleCase */
+// declare a process with an input of a string
+// have a list of words that you want to keep lowercased and assign it to storage space.
+// function titleCase(title) {
+//   var lowerCasedList = ['and', 'or', 'nor', 'and', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+//   for (var i = 0; i < title.length; i++) {
+//   }
+// }
