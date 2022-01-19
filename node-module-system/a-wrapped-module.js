d9e049d375fdb7abaecbbb46a012742f2b6228d9
Vickram Kahlon <vickram.kahlon@gmail.com>
2022-01-19 10:39:36 -0800

    Completed node-module-system.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('TypeOf exports:', typeof exports);
+console.log('Value of exports:', exports);
+
+console.log('TypeOf require:', typeof require);
+console.log('Value of require:', require);
+
+console.log('TypeOf module:', typeof module);
+console.log('Value of module:', module);
+
+console.log('TypeOf __filename:', typeof __filename);
+console.log('Value of __filename:', __filename);
+
+console.log('TypeOf __dirname:', typeof __dirname);
+console.log('Value of __dirname:', __dirname);
