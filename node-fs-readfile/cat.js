c92b27dc28c4f3c78984b1c078471d79f8a5485f
vkahlon <vickram.kahlon@gmail.com>
2022-01-19 21:42:26 -0800

    Patched up the optional challenge.

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
-for (var i = 2; i < process.argv.length; i++) {
-  fs.readFile(process.argv[i], 'utf8', (err, text) => {
+function getText(number) {
+  const fileCount = process.argv.length;
+  fs.readFile(process.argv[number], 'utf8', (err, text) => {
     if (err) throw err;
-    return console.log(text);
+    console.log(text);
   });
+  const i = number + 1;
+  if (i < fileCount) {
+    getText(i);
+  }
 }
+getText(2);
