8c7f2a3aec67024d52254e52193ebe8dad67a328
Vickram Kahlon <vickram.kahlon@gmail.org>
2022-01-25 21:23:30 +0000

    Finished express-post-json exercise.

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,12 +1,9 @@
 let nextID = 1;
 let grades = {};
+const gradesList = [];
 const express = require('express');
 const app = express();
 app.get('/api/grades', (req, res) => {
-  const gradesList = [];
-  for (const property in grades) {
-    gradesList.push(grades[property]);
-  }
   res.json(gradesList);
 });
 const newGrade = express.json();
@@ -16,9 +13,10 @@ app.post('/api/grades', (req, res) => {
   nextID++;
   res.json(req.body);
   grades = req.body;
+  gradesList.push(grades);
   res.send().status(201);
 });
-app.listen(8080, () => {
+app.listen(3000, () => {
   // eslint-disable-next-line no-console
-  console.log('Express server listening on port 8080');
+  console.log('Express server listening on port 3000');
 });
