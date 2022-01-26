b2b95b4a8935bf9a0d1a41e4f3e38533e3c1e4e7
vkahlon <vickram.kahlon@gmail.com>
2022-01-25 21:32:03 -0800

    Completed post section of exercise.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const fs = require('fs');
+const fs = require('fs');
 const data = require('./data.json');
 const app = express();
 
@@ -12,14 +12,43 @@ app.get('/api/notes', (req, res) => {
   res.status(200).json(notesList);
 });
 app.get('/api/notes/:id', (req, res) => {
-  // const getObject = data.notes;
-  const readID = req.params.id;
-  if (readID < 0) {
-    const invalidInput = { error: 'Invaid Number, must be above 0' };
-    res.sendStatus(404).json(invalidInput);
-  } // else if statement, if the condition is undefined, then respond with 404
+  const readObject = data.notes;
+  const readID = Number(req.params.id);
+  if ((readID < 1) || (!(Number.isInteger(readID)))) {
+    const invalidInput = { error: 'Invaid Number, id must be above positive or interger' };
+    res.status(400).json(invalidInput);
+  }
+  if (readObject[readID] !== undefined) {
+    res.status(200).json(readObject[readID]);
+  } else {
+    const emptyInput = { error: 'There is no note, with this ID' };
+    res.status(404).json(emptyInput);
+  }
 });
+const enableAppUse = express.json();
+app.use(enableAppUse);
 
+app.post('/api/notes', (req, res) => {
+  const newObject = req.body;
+  if (newObject.content === undefined) {
+    const emptyNote = { error: 'There is no note...' };
+    res.status(400).json(emptyNote);
+  }
+  let createData = data;
+  const grabID = createData.nextId;
+  newObject.id = grabID;
+  createData.notes[grabID] = newObject;
+  createData.nextId++;
+  createData = JSON.stringify(createData, null, 2);
+  fs.writeFile('data.json', createData, 'utf8', err => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.err(err);
+      res.status(500);
+    }
+  });
+  res.status(201).json(newObject);
+});
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 3000');
