befecc7718cc6317ba317fd6ddd4d451096a14db
Vickram Kahlon <vickram.kahlon@gmail.org>
2022-01-26 20:53:35 +0000

    Patched the challenge, added return statements when necessary and replace all err methods with error methods.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -9,21 +9,20 @@ app.get('/api/notes', (req, res) => {
   for (const property in getObject) {
     notesList.push(getObject[property]);
   }
-  res.status(200).json(notesList);
+  return res.status(200).json(notesList);
 });
 app.get('/api/notes/:id', (req, res) => {
   const readObject = data.notes;
   const readID = Number(req.params.id);
   if ((readID < 1) || (!(Number.isInteger(readID)))) {
     const invalidInput = { error: 'Invaid Number, id must be a positive interger' };
-    res.status(400).json(invalidInput);
+    return res.status(400).json(invalidInput);
   }
   if (readObject[readID] !== undefined) {
-    res.status(200).json(readObject[readID]);
-  }
-  if (readObject[readID] === undefined) {
+    return res.status(200).json(readObject[readID]);
+  } else {
     const emptyInput = { error: `There is no note, with ${readID} as its ID` };
-    res.status(404).json(emptyInput);
+    return res.status(404).json(emptyInput);
   }
 });
 const enableAppUse = express.json();
@@ -33,7 +32,7 @@ app.post('/api/notes', (req, res) => {
   const newObject = req.body;
   if (newObject.content === undefined) {
     const emptyNote = { error: 'There is no note...' };
-    res.status(400).json(emptyNote);
+    return res.status(400).json(emptyNote);
   }
   if (newObject.content !== undefined) {
     let createData = data;
@@ -44,12 +43,11 @@ app.post('/api/notes', (req, res) => {
     createData = JSON.stringify(createData, null, 2);
     fs.writeFile('data.json', createData, 'utf8', err => {
       if (err) {
-        // eslint-disable-next-line no-console
-        console.err(err);
+        console.error(err);
         const backEndError = { error: 'An unexpected error occured' };
-        res.status(500).json(backEndError);
+        return res.status(500).json(backEndError);
       }
-      res.status(201).json(newObject);
+      return res.status(201).json(newObject);
     });
   }
 });
@@ -58,7 +56,7 @@ app.delete('/api/notes/:id', (req, res) => {
   const deleteID = Number(req.params.id);
   if ((deleteID < 1) || (!(Number.isInteger(deleteID)))) {
     const invalidInput = { error: 'Invaid Number, id must be a positive interger' };
-    res.status(400).json(invalidInput);
+    return res.status(400).json(invalidInput);
   }
   if (deleteObject[deleteID] !== undefined) {
     let deleteData = data;
@@ -66,16 +64,15 @@ app.delete('/api/notes/:id', (req, res) => {
     deleteData = JSON.stringify(deleteData, null, 2);
     fs.writeFile('data.json', deleteData, 'utf8', err => {
       if (err) {
-        // eslint-disable-next-line no-console
-        console.err(err);
+        console.error(err);
         const backEndError = { error: 'An unexpected error occured' };
-        res.status(500).json(backEndError);
+        return res.status(500).json(backEndError);
       }
-      res.status(201).json();
+      return res.status(201).json();
     });
   } else {
     const emptyInput = { error: `There is no note, with ${deleteID} as its ID` };
-    res.status(404).json(emptyInput);
+    return res.status(404).json(emptyInput);
   }
 });
 app.put('/api/notes/:id', (req, res) => {
@@ -84,15 +81,15 @@ app.put('/api/notes/:id', (req, res) => {
   const editID = Number(req.params.id);
   if ((editID < 1) || (!(Number.isInteger(editID)))) {
     const invalidInput = { error: 'Invaid Number, id must be a positive interger' };
-    res.status(400).json(invalidInput);
+    return res.status(400).json(invalidInput);
   }
   if ((editObject[editID] !== undefined) && (editNestedObject.content === undefined)) {
     const invalidContent = { error: `Missing content, ${editID} is a valid ID, try again with but have content message.` };
-    res.status(404).json(invalidContent);
+    return res.status(404).json(invalidContent);
   }
   if ((editObject[editID] === undefined) && (editNestedObject.content !== undefined)) {
     const invalidContent = { error: `ID: ${editID} is not valid, system recognizes content message, try again with a valid ID.` };
-    res.status(404).json(invalidContent);
+    return res.status(404).json(invalidContent);
   }
   if ((editObject[editID] !== undefined) && (editNestedObject.content !== undefined)) {
     editNestedObject.ID = editID;
@@ -101,12 +98,11 @@ app.put('/api/notes/:id', (req, res) => {
     replaceData = JSON.stringify(replaceData, null, 2);
     fs.writeFile('data.json', replaceData, 'utf8', err => {
       if (err) {
-        // eslint-disable-next-line no-console
-        console.err(err);
+        console.error(err);
         const backEndError = { error: 'An unexpected error occured' };
-        res.status(500).json(backEndError);
+        return res.status(500).json(backEndError);
       }
-      res.status(201).json(editNestedObject);
+      return res.status(201).json(editNestedObject);
     });
   }
 });
