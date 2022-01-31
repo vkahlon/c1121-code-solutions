168435dc8c7060082be859581717a518767c17fd
Vickram Kahlon <vickram.kahlon@gmail.org>
2022-01-31 18:23:56 +0000

    Completed array filter exercise.

---
+++ array-filter/main.js
@@ -0,0 +1,24 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna'
+];
+const evenNumbers = numbers.filter(evenNumber => evenNumber % 2 === 0);
+console.log(`List of Even Numbers: ${evenNumbers}`);
+
+const overFive = numbers.filter(thefive => thefive > 5);
+console.log(`List of Numbers over 5: ${overFive}`);
+
+const thoseWithE = names.filter(letterE => (letterE.startsWith('E')));
+console.log(`The ones with E: ${thoseWithE}`);
+
+const includesDoubleD = names.filter(doubleD => (doubleD.includes('D', 'd')));
+console.log(`The ones with the letter d: ${includesDoubleD}`);
