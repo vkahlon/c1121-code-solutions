abb03287c832e808e2b33d05f3079274c48f9262
Vickram Kahlon <vickram.kahlon@gmail.org>
2022-02-04 01:36:44 +0000

    Added shrek.

--- react-toggle-switch-component/src/index.jsx
+++ react-toggle-switch-component/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HotButton from './toggle-switch';
+import SwampSwitch from './toggle-switch';
 
 ReactDOM.render(
-  < HotButton />,
+  < SwampSwitch />,
   document.querySelector('#root')
 );
--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-export default class HotButton extends React.Component {
+export default class SwampSwitch extends React.Component {
   constructor(props) {
     super(props);
     this.state = { theStatus: false };
@@ -14,16 +14,26 @@ export default class HotButton extends React.Component {
     const status = this.state.theStatus;
     let layoutClass = 'onLayout';
     let inputClass = 'toggleOn';
+    let shrek = 'https://faceswaponline.com/wp-content/uploads/2017/10/shrek-yelling.png';
+    let shout = 'GET OUT OF MY SWAMP';
+    let enviorment = 'container swamp';
     if (!status) {
       layoutClass = 'offLayout';
       inputClass = 'toggleOff';
+      shrek = '';
+      shout = '';
+      enviorment = 'container';
     }
     return (
-      <div className='container'>
+      <div className={enviorment}>
       <div className='row'>
       <div className={layoutClass}>
         <input onClick={this.handleClick} type="checkbox" className={inputClass} />
       </div>
+      </div>
+        <div className='row shrek'>
+        <img src={shrek} />
+        <h2>{shout}</h2>
       </div>
       </div>
     );
