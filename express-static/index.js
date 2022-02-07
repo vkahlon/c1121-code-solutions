7a14625d78269401526529bad36849543b073a88
Vickram Kahlon <vickram.kahlon@gmail.org>
2022-02-07 18:47:20 +0000

    Patched express.static and path.join.

--- express-static/index.js
+++ express-static/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const combinePaths = path.join('./public', __dirname);
+const combinePaths = path.join(__dirname, 'public');
 console.log(combinePaths);
-const testing = express.static('public');
+const testing = express.static(combinePaths);
 app.use(testing);
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
