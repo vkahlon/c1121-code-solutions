8aa793400228b5869f444ad9e04abd6a2c2dcb08
vkahlon <vickram.kahlon@gmail.com>
2022-02-08 22:07:00 -0800

    Made nextImage improvements, removed the two unecessary methods.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,56 +3,42 @@ export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = { image: 0 };
-    this.handleRightClick = this.handleRightClick.bind(this);
-    this.handleLeftClick = this.handleLeftClick.bind(this);
     this.handleCircleClick = this.handleCircleClick.bind(this);
     this.nextImage = this.nextImage.bind(this);
-  }
-
-  handleRightClick() {
-    clearInterval(this.timer);
-    const currentView = this.state.image;
-    const limit = this.props.prop.length - 1;
-    if (currentView === limit) {
-      this.setState({ image: 0 });
-    } else {
-      this.setState({ image: this.state.image + 1 });
-    }
-    this.nextImage();
-  }
-
-  handleLeftClick() {
-    clearInterval(this.timer);
-    const currentView = this.state.image;
-    const limit = this.props.prop.length - 1;
-    if (currentView === 0) {
-      this.setState({ image: limit });
-    } else {
-      this.setState({ image: this.state.image - 1 });
-    }
-    this.nextImage();
   }
 
   handleCircleClick(id) {
     clearInterval(this.timer);
     this.setState({ image: id });
-    this.nextImage();
+    this.timer = setInterval(this.nextImage, 1000);
   }
 
   componentDidMount() {
-    this.nextImage();
+    this.timer = setInterval(this.nextImage, 1000);
   }
 
-  nextImage() {
+  nextImage(direction) {
+    if (direction !== undefined) {
+      clearInterval(this.timer);
+    }
+    const currentView = this.state.image;
     const limit = this.props.prop.length - 1;
-    this.timer = setInterval(() => {
-      const currentView = this.state.image;
-      if (currentView === limit) {
-        return this.setState({ image: 0 });
+    if (direction === 'left') {
+      if (currentView === 0) {
+        this.setState({ image: limit });
       } else {
-        return this.setState({ image: this.state.image + 1 });
+        this.setState({ image: this.state.image - 1 });
       }
-    }, 1000);
+    } else {
+      if (currentView === limit) {
+        this.setState({ image: 0 });
+      } else {
+        this.setState({ image: this.state.image + 1 });
+      }
+    }
+    if (direction !== undefined) {
+      this.timer = setInterval(this.nextImage, 1000);
+    }
   }
 
   render() {
@@ -68,13 +54,13 @@ export default class Carousel extends React.Component {
       <div className='container outer-decoration'>
         <div className='row quick-wrapper'>
           <div className='column-twenty'>
-            <i onClick={this.handleLeftClick}className='fas fa-angle-left row icon-wrapper'></i>
+            <i onClick={() => { this.nextImage('left'); }}className='fas fa-angle-left row icon-wrapper'></i>
           </div>
           <div className='column-eighty'>
             <img className='img-wrapper' src={this.props.prop[image].image} alt='logo' />
           </div>
           <div className='column-twenty'>
-            <i onClick={this.handleRightClick}className='fas fa-angle-right row icon-wrapper'></i>
+            <i onClick={() => { this.nextImage('right'); }}className='fas fa-angle-right row icon-wrapper'></i>
           </div>
         </div>
         <div className='row quick-wrapper'>
