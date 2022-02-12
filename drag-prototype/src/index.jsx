cd3085b39fed8ee121c26c682b2606e73a943c94
Vickram Kahlon <vickram.kahlon@gmail.org>
2022-02-12 00:24:47 +0000

    Entered data

--- drag-prototype/src/index.jsx
+++ drag-prototype/src/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DragAndDrop from './DragAndDrop';
-const units = null;
+import intialData from '../library/intialData';
+import Column from './Column';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = intialData;
+  }
+
+  render() {
+    return this.state.columnOrder.map(columnId => {
+      const column = this.state.columns;
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+      return <Column key={column.id} column={column} tasks={tasks}/>;
+    });
+  }
+}
 ReactDOM.render(
-  < DragAndDrop prop={units}/>,
+  < App />,
   document.querySelector('#root')
 );
