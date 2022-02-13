409628acc7bd3814d5e9a4a3b3b456dfe506ad08
vkahlon <vickram.kahlon@gmail.com>
2022-02-12 17:46:27 -0800

    Implemented draging

--- drag-prototype/src/index.jsx
+++ drag-prototype/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DragDropContext } from 'react-beautiful-dnd';
 import intialData from '../library/intialData';
 import Column from './Column';
 
@@ -7,14 +8,24 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = intialData;
+    this.onDragEnd = this.onDragEnd.bind(this);
+
+  }
+
+  onDragEnd() {
+    return result => {
+      // TODO: reorder our column
+    };
   }
 
   render() {
-    return this.state.columnOrder.map(columnId => {
-      const column = this.state.columns;
-      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-      return <Column key={column.id} column={column} tasks={tasks}/>;
-    });
+    return <DragDropContext onDragEnd={this.onDragEnd}>
+     {this.state.columnOrder.map(columnId => {
+       const column = this.state.columns[columnId];
+       const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+       return <Column key={column.id} column={column} tasks={tasks}/>;
+     })};
+    </DragDropContext>;
   }
 }
 ReactDOM.render(
