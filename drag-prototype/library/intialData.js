3462e23e3a3b614356fef1543101b6e4f50d071e
vkahlon <vickram.kahlon@gmail.com>
2022-02-13 16:07:15 -0800

    Drag has been implemented

--- drag-prototype/library/intialData.js
+++ drag-prototype/library/intialData.js
@@ -1,18 +1,24 @@
 const initialData = {
   tasks: {
-    'task-1': { id: 'task-1', content: 'Take out the garbage' },
-    'task-2': { id: 'task-2', content: 'Watch my favorite show' },
-    'task-3': { id: 'task-3', content: 'Charge my phone' },
-    'task-4': { id: 'task-4', content: 'Cook dinner' }
+    'task-1': { id: 'task-1', content: 'Mario', strength: 500 },
+    'task-2': { id: 'task-2', content: 'Snake', strength: 500 },
+    'task-3': { id: 'task-3', content: 'Link', strength: 500 },
+    'task-4': { id: 'task-4', content: 'Byleth', strength: 500 }
   },
   columns: {
     'column-1': {
       id: 'column-1',
-      title: 'To do',
+      title: 'Units',
       taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+    },
+    'column-2': {
+      id: 'column-2',
+      title: 'Squad',
+      taskIds: []
     }
   },
-  // Faciliate reordering of the columns
-  columnOrder: ['column-1']
+  // Facilitate reordering of the columns
+  columnOrder: ['column-1', 'column-2']
 };
+
 export default initialData;
--- drag-prototype/src/index.jsx
+++ drag-prototype/src/index.jsx
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import { DragDropContext } from 'react-beautiful-dnd';
 import intialData from '../library/intialData';
 import Column from './Column';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  display: flex;
+`;
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = intialData;
   }
+
+  onDragUpdate = update => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(this.state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba( 153, 141, 217, ${opacity})`;
+  };
     // eslint-disable-next-line
   onDragEnd = result => {
     const { destination, source, draggableId } = result;
@@ -24,35 +36,72 @@ class App extends React.Component {
       return;
     }
 
-    const column = this.state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
 
-    const newColumn = {
-      ...column,
-      taskIds: newTaskIds,
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      this.setState(newState);
+      return;
+    }
+
+    // Moving from one list to another
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
     };
 
     const newState = {
       ...this.state,
       columns: {
         ...this.state.columns,
-        [newColumn.id]: newColumn,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
       },
     };
-
     this.setState(newState);
   };
 
   render() {
-    return <DragDropContext onDragEnd={this.onDragEnd}>
-     {this.state.columnOrder.map(columnId => {
-       const column = this.state.columns[columnId];
-       const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-       return <Column key={column.id} column={column} tasks={tasks}/>;
-     })};
-    </DragDropContext>;
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Container>
+          {this.state.columnOrder.map(columnId => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(
+              taskId => this.state.tasks[taskId],
+            );
+
+            return <Column key={column.id} column={column} tasks={tasks} />;
+          })}
+        </Container>
+      </DragDropContext>
+    );
   }
 }
 ReactDOM.render(
